656ba68b9efe554321a0e9277589bc379230a337
Daylin Baldwin <70437477+corruptedmemes@users.noreply.github.com>
2022-02-04 09:24:04 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Loretta.CodeAnalysis;
+using Loretta.CodeAnalysis.Lua;
+
+namespace LuaBeautifier
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var file = File.ReadAllText("input.lua");
+
+            var parsed = LuaSyntaxTree.ParseText(file);
+
+            File.WriteAllText("output.lua", parsed.GetRoot().NormalizeWhitespace().ToFullString());
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LuaBeautifier")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LuaBeautifier")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ECB1BD72-4338-4881-A91E-21B54BDEB9AA")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
